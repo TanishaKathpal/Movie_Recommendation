3f640b123c54a7b9d864339ec5314a2623d9c82a
TanishaKathpal <tanisha190405@gmail.com>
2025-07-15 23:16:39 +0530

    updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pickle
+import joblib  
 import pandas as pd
 
 st.set_page_config(page_title="Movie Recommender", layout="centered")
@@ -7,17 +7,14 @@ st.set_page_config(page_title="Movie Recommender", layout="centered")
 st.markdown("""
     <style>
         .stApp {
-            
             background-color: #0f172a;
             color: #e2e8f0;
         }
-
         .stSelectbox label {
             color: #00ffff;
             font-weight: bold;
             font-size: 16px;
         }
-
         .stButton>button {
             background-color: #00ffff;
             color: #0f172a;
@@ -29,16 +26,13 @@ st.markdown("""
             margin-top: 10px;
             transition: 0.3s ease;
         }
-
         .stButton>button:hover {
             background-color: gray;
             color: white;
         }
-
         h1, h3 {
             text-align: center;
         }
-
         .recommend-title {
             color: #00ffff;
         }
@@ -49,12 +43,11 @@ st.markdown("<h1 class='recommend-title'>Movie Recommendation System</h1>", unsa
 st.markdown("<p style='text-align: center;'>Select a movie and we'll recommend some great ones!</p>", unsafe_allow_html=True)
 
 # Load data
-movies = pickle.load(open('movies.pkl', 'rb'))
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+movies = joblib.load(open('movies.pkl', 'rb'))  
+similarity = joblib.load('similarity.pkl')       
 movies_list = movies['title'].values
 
 selected_movie = st.selectbox("Choose a movie:", movies_list)
-
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
@@ -63,11 +56,11 @@ def recommend(movie):
     recommended_movies = [movies.iloc[i[0]].title for i in movie_indices]
     return recommended_movies
 
-
 if st.button("Recommend"):
     recommendations = recommend(selected_movie)
     st.markdown("### Top Recommendations:")
     for idx, title in enumerate(recommendations, 1):
         st.markdown(f"{idx}. **{title}**")
 
-# poster fetch:future enhancements
+# poster fetch: future enhancement
+
